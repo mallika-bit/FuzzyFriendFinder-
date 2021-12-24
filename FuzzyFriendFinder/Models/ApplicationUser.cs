f52f095b7b320d7fb7e55dcb3b2dbb3c406d82aa
Your Name <gsk2021@gmail.com>
2021-12-24 16:01:04 -0500

    push

--- FuzzyFriendFinder/Models/ApplicationUser.cs
+++ FuzzyFriendFinder/Models/ApplicationUser.cs
@@ -17,7 +17,7 @@ namespace FuzzyFriendFinder.Models
            
            
           
-           public string PhoneNumber { get; set; }
+          // public string PhoneNumber { get; set; }
    
             public string Address { get; set; }
 
